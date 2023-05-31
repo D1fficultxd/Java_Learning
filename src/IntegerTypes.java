d87a0d9ac3451b2f7f0f13b621a9c078fa783ba2
d1fficult <dayn2803@gmail.com>
2023-05-31 23:47:40 +0300

    commit

---
+++ src/IntegerTypes.java
@@ -0,0 +1,15 @@
+public class IntegerTypes {
+    public static void main(String[] args) {
+        // int speed = 300_000;
+        // Неявное приведение типа long distance = 365 * 24 * 60 * 60 * speed;
+    //    System.out.println(distance);
+
+       // byte b = 10;
+      // Явное приведение типа  b = (byte) (b + 10);
+       // b += 1;
+       // b++;
+       // System.out.println(b);
+        int a = 1;
+        byte b = (byte) a;
+    }
+}
