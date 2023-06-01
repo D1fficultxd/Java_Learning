4e17334a9017d686799fb267686ef563828a5833
D1fficult <dayn2803@gmail.com>
2023-06-01 11:20:40 +0300

    Data Types added

---
+++ src/FloatDouble.java
@@ -0,0 +1,13 @@
+public class FloatDouble {
+    public static void main(String[] args) {
+//        float radius = 10.8f;
+////        float pi = 3.14f;
+////        float area = pi * radius * radius;
+////        System.out.println(area);
+        int Ivan = 14;
+        int Natasha = 50;
+        int Sasha = 60;
+        float sum = (float) (Ivan+Natasha+Sasha) / 3;
+        System.out.println(sum);
+    }
+}
