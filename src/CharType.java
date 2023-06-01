049ecd7178b4789ce20eaed672f960eb4277d84c
D1fficult <dayn2803@gmail.com>
2023-06-01 11:35:32 +0300

    Data Types added

---
+++ src/CharType.java
@@ -0,0 +1,9 @@
+public class CharType {
+    public static void main(String[] args) {
+        char x = 88;
+        char x1 = 'X';
+        x1++;
+        System.out.println("Value of x: " + x);
+        System.out.println("Value of x1: " + x1);
+    }
+}
