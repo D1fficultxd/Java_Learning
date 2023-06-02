9d4c9fd4f4db3086933e796ccef9e56cc95e56e4
D1fficult <dayn2803@gmail.com>
2023-06-02 20:26:29 +0300

    Data Types added

---
+++ src/Strings.java
@@ -0,0 +1,11 @@
+public class Strings {
+    public static void main(String[] args) {
+//        String s = "Hello World";
+//        String d = "!";
+//        System.out.println(s+d);
+        String name = "Ivan";
+        int age = 14;
+        String sum = "Hello " + name + " \nyoure " + age;
+        System.out.println(sum);
+    }
+}
