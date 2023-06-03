3397af7b0de3744eae697cf0055a103dc137e390
d1fficult <dayn2803@gmail.com>
2023-06-03 22:00:18 +0300

    commit

---
+++ src/Array.java
@@ -0,0 +1,31 @@
+public class Array {
+    public static void main(String[] args) {
+//        String[] namesOfMonths = new String[12];
+//        namesOfMonths[0] = "January";
+//        namesOfMonths[1] = "February";
+//        namesOfMonths[2] = "March";
+//        namesOfMonths[3] = "April";
+//        namesOfMonths[4] = "May";
+//        namesOfMonths[5] = "June";
+//        namesOfMonths[6] = "July";
+//        namesOfMonths[7] = "August";
+//        namesOfMonths[8] = "September";
+//        namesOfMonths[9] = "October";
+//        namesOfMonths[10] = "November";
+//        namesOfMonths[11] = "December";
+//        for(int i = 0; i < namesOfMonths.length; i++){
+//            System.out.print(namesOfMonths[i]);
+//            if (i < namesOfMonths.length - 1){
+//                System.out.print(", ");
+//            } else {
+//                System.out.print(".");
+//            }
+//        }
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = numbers.length - 1; i >= 0; i--){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
---
+++ src/ForEach.java
@@ -0,0 +1,8 @@
+public class ForEach {
+    public static void main(String[] args) {
+//        String[] array = {"Nigger", "Black", "Slave", "Jonh"};
+//        for (int i = 0; i< array.length; i++){
+//            System.out.println(array[i]);
+//        }
+    }
+}
